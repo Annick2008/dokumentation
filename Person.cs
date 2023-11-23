92a284159f9dd5c6e06b5dae6c7126d5ab0816fa
Yannick Kreuter <y.kreuterle@sbg.at>
2023-11-23 09:25:47 +0100

    add documentation

--- Person.cs
+++ Person.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace dokumentation
 {
+    /// <summary>
+    /// Person kann erstellt werden
+    /// </summary>
     public class Person
     {
     }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using dokumentation;
+
+Person p1 = new Person();
+
+Person p2 = new Person();
